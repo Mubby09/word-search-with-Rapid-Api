7469918ddff44e63f65eaf01bbe28d0425abde5c
Mubarak <akinsolamubarak1996@gmail.com>
2020-11-18 17:13:14 +0000

    changes made for error timeout on invalid word

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const submit = document.getElementById("submit"),
   search = document.getElementById("search"),
-  //   resultHeading = document.getElementById("result-heading"),
+  resultHeading = document.getElementById("result-heading"),
   wordEl = document.getElementById("words"),
   singleMealEl = document.getElementById("single-meal");
 
@@ -23,70 +23,29 @@ function searchMeal(e) {
         wordEl.innerHTML = response.results.map(
           (result) =>
             `<div>
-               <div ='${result.definition}'>
-            <h5>PART OF SPEECH : ${result.partOfSpeech}</h5>
-            <h3>${result.definition}</h3>
-          </div>
-          </div>
-               `
+                 <div ='${result.definition}'>
+              <h5>PART OF SPEECH : ${result.partOfSpeech}</h5>
+              <h3>${result.definition}</h3>
+            </div>
+            </div>
+                 `
         );
         console.log(response);
+        setTimeout(() => {
+          window.location.reload();
+        }, 50000);
         console.log(response.results);
       })
       .catch((err) => {
         console.error(err);
+        // resultHeading.innerHTML = `<h2>Nothing Found for '${term.toUpperCase()}'<h2>`;
+        resultHeading.innerHTML = `<h2>PLEASE INPUT A CORRECT ENGLISH WORD<h2>`;
+
+        setTimeout(function() {
+          window.location.reload(1);
+        }, 1500);
       });
   }
 }
-
-// fetch(`https://wordsapiv1.p.rapidapi.com/words/{term}`)
-//   .then((res) => res.json())
-//   .then((data) => {
-//     console.log(data);
-// resultHeading.innerHTML = `<h2>Search results for '${term.toUpperCase()}':<h2>`;
-// if (data.meals === null) {
-//   resultHeading.innerHTML = `<h2>Nothing Found for '${term.toUpperCase()}'<h2>`;
-//           wordEl.innerHTML = "";
-//         } else {
-//           wordEl.innerHTML = data.meals
-//             .map(
-//               (meal) =>
-//                 `<div class='meal'>
-//               <img src ='${meal.strMealThumb}' alt='${meal.strMeal}'/ >
-//               <div class='meal-info' data-mealID ='${meal.idMeal}'>
-//               <h3>${meal.strMeal}</h3>
-//            </div>
-//           </div>
-//               `
-//             )
-//             .join("");
-//         }
-//       });
-//     //clear search text.
-//     search.value = "";
-//   } else {
-//     const div = document.createElement("div");
-//     div.className = `alert`;
-//     div.appendChild(document.createTextNode("Please type a word"));
-//     const container = document.querySelector(".container");
-//     const before = document.querySelector(".flex");
-//     container.insertBefore(div, before);
-//     timeOut();
-//   }
-// }
-
-// function timeOut() {
-//   setTimeout(() => {
-//     clearAlert();
-//     // window.location.reload(true);
-//   }, 2000);
-// }
-
-// function clearAlert() {
-//   const currentAlert = document.querySelector(".alert");
-//   if (currentAlert) {
-//     currentAlert.remove();
-//   }
-// }
 
 submit.addEventListener("submit", searchMeal);
